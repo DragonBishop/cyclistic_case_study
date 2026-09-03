1d37fe2ca8ab615e6e58f506de3fb05c59177566
DragonBishop <276789280+DragonBishop@users.noreply.github.com>
2026-09-03 01:57:13 -0400

    refactor: streamline data analysis functions and improve code readability

--- src/data/PostGISConnector.py
+++ src/data/PostGISConnector.py
@@ -7,15 +7,18 @@ from sqlalchemy import URL
 
 class PostGISConnector:
     """Assembles a PostgreSQL connection address, then queries the database"""
-    def __init__(self, host="localhost", port=5432, database="data_science", username="postgres") -> None:
-        self.drivername="postgresql+psycopg"
+
+    def __init__(
+        self, host="localhost", port=5432, database="data_science", username="postgres"
+    ) -> None:
+        self.drivername = "postgresql+psycopg"
         self.host = host
         self.port = port
         self.database = database
         self.username = username
         self.password = getpass.getpass("Password: ")
         self.engine = self._create_engine()
-    
+
     def _create_engine(self) -> sql.Engine:
         """Uses PostGISConnector attributes to return database address."""
         address = URL.create(
@@ -24,8 +27,9 @@ class PostGISConnector:
             password=self.password,
             host=self.host,
             port=self.port,
-            database=self.database)
-        
+            database=self.database,
+        )
+
         return sql.create_engine(address)
 
     def postgis_query(self, query: str, geom_col: str) -> gpd.GeoDataFrame:
@@ -34,13 +38,15 @@ class PostGISConnector:
             sql=query,
             con=self.engine,
             # NOTE: GeoDataFrame can only analyze one geometry column at a time
-            geom_col=geom_col)
+            geom_col=geom_col,
+        )
         return capstone_data_raw
+
 
 if __name__ == "__main__":
     connector = PostGISConnector()
     df = connector.postgis_query(
-        query="SELECT * FROM gda_capstone_a_raw.may2025_may2026_cleaned LIMIT 5", 
-        geom_col="start_coordinates"
+        query="SELECT * FROM gda_capstone_a_raw.may2025_may2026_cleaned LIMIT 5",
+        geom_col="start_coordinates",
     )
-    print(df.head())
+    print(df.head())
